364270715845e07e9555076b539d7c65959e379b
njlaoyang <yxyang@berkeley.edu>
2015-11-22 21:55:45 -0800

    some seemingly better method added

--- brute-force/brute-force/src/temp.java
+++ brute-force/brute-force/src/temp.java
@@ -1,16 +1,34 @@
+import java.util.Random;
 
 public class temp {
 	public static void main(String[] args) {
+		Random rnd = new Random();
 		int n = 100;
+		int[][] g = new int[n][n];
+		int[] ord = new int[n];
 		System.out.println(1);
 		System.out.println(n);
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				if (j == i+1) {
-					System.out.print("1 ");					
+				if (rnd.nextBoolean()) {
+					g[i][j] = 1;
 				} else {
-					System.out.print("0 ");
+					g[i][j] = 0;
 				}
+			}
+			ord[i] = i;
+		}
+		
+		for (int i = 0; i < n; i++) {
+			int ind = rnd.nextInt(100);
+			int t = ord[i];
+			ord[i] = ord[ind];
+			ord[ind] = t;
+		}
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print(g[ord[i]][ord[j]] + " ");
 			}
 			System.out.println();
 		}
